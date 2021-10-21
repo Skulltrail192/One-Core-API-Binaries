67204fc8966f9beb67f4c5dc913eaa5bfb8f6716
skulltrail <samuel_4186@hotmail.com>
2021-10-21 00:20:39 -0300

    Disabling kernel update installer generation on script

--- Packages/Release/Buildx64.bat
+++ Packages/Release/Buildx64.bat
@@ -5,7 +5,6 @@ set output=D:\Develop\One-Core\Output\Binaries\x64
 set api-set=API-SET Installer
 set base=Base Installer
 set compat=App Compat Installer
-set kernel=kernel Installer
 set kernel-alone=kernel Installer Standalone
 set d3d=D3D Installer
 set new=New Dlls Installer
@@ -14,7 +13,6 @@ REM set shell=Shell Update Installer
 set api-set-exe=one-core-api-api-set.exe
 set base-exe=one-core-api-app-base.exe
 set compat-exe=one-core-api-app-compat.exe
-set kernel-exe=one-core-api-app-kernel.exe
 set kernel-alone-exe=one-core-api-app-kernel-alone.exe
 set d3d-exe=one-core-api-d3d.exe
 set new-exe=one-core-api-new.exe
@@ -23,7 +21,6 @@ REM set shell-exe=one-core-api-shell.exe
 SFXCAB.exe "%output%\%api-set-exe%" "%root%\%api-set%" -r:update/update.exe -ipd -iswu
 SFXCAB.exe "%output%\%base-exe%" "%root%\%base%" -r:update/update.exe -ipd -iswu
 SFXCAB.exe "%output%\%compat-exe%" "%root%\%compat%" -r:update/update.exe -ipd -iswu
-SFXCAB.exe "%output%\%kernel-exe%" "%root%\%kernel%" -r:update/update.exe -ipd -iswu
 SFXCAB.exe "%output%\%kernel-alone-exe%" "%root%\%kernel-alone%" -r:update/update.exe -ipd -iswu
 SFXCAB.exe "%output%\%d3d-exe%" "%root%\%d3d%" -r:update/update.exe -ipd -iswu
 SFXCAB.exe "%output%\%new-exe%" "%root%\%new%" -r:update/update.exe -ipd -iswu
